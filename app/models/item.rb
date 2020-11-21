227c568a9a6f4e9a37f64822afff32e76c5ac5e2
yamakazu88 <yamakazu.9683@gmail.com>
2020-11-21 11:19:42 +0900

    商品出品ページの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,26 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :status
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :image
+
+    with_options numericality: { other_than: 1 } do
+      validates :genre_id
+      validates :status_id
+      validates :charge_id
+      validates :prefecture_id
+      validates :day_id
+    end
+  end
+
 end
