3a676f9cce4274bba56feee8186458a4fbf25d87
yamakazu88 <yamakazu.9683@gmail.com>
2020-11-23 10:37:14 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController
   def index
-    
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+   private
+  def item_params
+    params.require(:item).permit(:name, :text, :genre_id, :status_id, :charge_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
