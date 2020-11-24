89fa98a73ee437bfb694eb31878911ac91a75aea
yamakazu88 <yamakazu.9683@gmail.com>
2020-11-24 17:03:02 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user.id || @item.log.present?
+    if current_user.id == @item.user.id || @item.log.present?
       redirect_to root_path
     end
   end
