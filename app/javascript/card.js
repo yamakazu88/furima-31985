36b575f710cb3e9eb14c61be6b6eb6e7e78e7ef0
yamakazu88 <yamakazu.9683@gmail.com>
2020-11-24 11:38:39 +0900

    トークンをサーバーサイドへ送る

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -17,10 +17,19 @@ const pay = () => {
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+
+      document.getElementById("order_number").removeAttribute("name");
+      document.getElementById("order_cvc").removeAttribute("name");
+      document.getElementById("order_exp_month").removeAttribute("name");
+      document.getElementById("order_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
-    
+
   });
 };
 
