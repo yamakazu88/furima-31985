f2d7236d4a87514e92beb63777571ad8bd7c4dce
yamakazu88 <yamakazu.9683@gmail.com>
2020-12-01 16:00:16 +0900

    コメント機能のコントローラーとルーティングの設定（仮）

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :logs, only: [:index, :create]
+    resources :logs,     only: [:index, :create]
+    resources :comments, only: [:create]
   end
 end
