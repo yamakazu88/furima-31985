3b82e8f38d4b2e51959f20229f1b814fc1133faf
yamakazu88 <yamakazu.9683@gmail.com>
2020-12-22 16:18:50 +0900

    index.jsの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("channels")
 require("../item_price");
 require("../card")
 require('./preview')
-require("../index")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
